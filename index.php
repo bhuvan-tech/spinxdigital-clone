8e94275ad3db769753115095e7dd7c741d3ab3ed
Bhuvan Y E <74106022+bhuvan-tech@users.noreply.github.com>
2021-10-28 19:06:56 +0530

    This is the clone website of spinxdigital

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SpinxDigital-Clone</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    
+    <section class="one">
+        <nav class="first">
+            <span>
+                <img src="images/spinx-logo.png" alt="logo"></img>
+
+            </span>
+            <ul>
+                <li><a href="#">work</a></li>
+                <li><a href="#">expertise</a></li>
+                <li><a href="#">process</a></li>
+                <li><a href="#">agency</a></li>
+                <li><a href="#">resources</a></li>
+                <li><a href="#" id="contact-info">contact</a></li>
+    
+            </ul>
+        </nav>
+        <div class="bigheading">
+            <div id="big">BIG IDEAS,</div>
+            <div id="big">CREATIVE PEOPLE,</div>
+            <div id="big">NEW TECHNOLOGY.</div>
+        </div>
+    </section>
+    <section class="two">
+        <section class="s2d1">
+            <div class="big-heading">
+                <h1>THINK</h1>
+                <h1 id="gre">BIG.</h1>
+            </div>
+            <h2>Web Design Company in Los Angeles</h2>
+            <p>
+                At SPINX Digital, our team of innovators and digital marketers in tech and design bring skills above and beyond the ordinary to every project. Our Los Angeles web design and development team is involved in professional B2B and B2C web design and development, mobile applications, digital strategy, user experience, advertising, social media, content management systems, and email marketing initiatives.
+            </p>
+            <p id="gre">AWARD WINNING DIGITAL AGENCY</p>
+        </section>
+        <section class="s2d2">
+            <div class="line">
+                <div class="head2">PHYSICIATIONS FORMULA</div>
+                <div>
+                    <img src="images/icon-arrow-green.svg" alt="images">
+                    <img src="images/icon-arrow-green.svg" alt="images">
+                </div>
+            </div>
+            <div class="imgover">
+                <img src="images/best_in_class_2016_round.svg">
+            </div>
+            <!-- <img src="images/image-PF.jpg"> -->
+            
+        </section>
+        
+    </section>
+    <section class="three">
+        <div>
+            <div class="forgreen">
+                <h1>RECENT WORK</h1>
+            </div>
+            
+            <p>We have a deep understanding of user habits and behaviors. We are committed to using fact-based knowledge and our unique brand of innovation to help you dominate the competition. Every project is customized for optimum visibility and audience response.</p>
+        </div>
+        <div class="imgs">
+            <div>
+                <img src="images/image-qe.jpg" id="exp" alt="">
+            </div>
+            <div>
+                <img src="images/image-AIYA.jpg" id="exp" alt="">
+            </div>
+            <div>
+                <img src="images/we-believe-idea-bg.jpg"  alt="">
+            </div>
+            <div>
+                <img src="images/image-qe.jpg" id="exp" alt="">
+            </div>
+            <div>
+                <img src="images/image-AIYA.jpg" id="exp" alt="">
+            </div>
+            <div>
+                <img src="images/we-believe-idea-bg.jpg" alt="">
+            </div>
+        </div>   
+        
+           
+    </section>
+    <section class="four">
+        <div class="foot1">
+            <h1 id="gre">CONTACT US</h1>
+            <form action="registeration_form.php" method="post">
+                <ul>
+                    <li>First Name * <br><br> <input type="name" placeholder="Name"></li>
+                    <li>Last Name * <br><br> <input type="name" placeholder="Name"></li>
+                    
+                    <li>Email * <br><br><input type="email" placeholder="Email"></li>
+                    <li>Company Name <br><br><input type="email" placeholder="Email"></li>
+                    <li>Phone Number: <br><br><input type="phone number" placeholder="Phone Number"></li>
+
+                    <li>Project Type Product * <br><br>
+                        <select name="Project Type Product" aria-placeholder="Please Select">
+                            
+                        </select>
+                    </li>
+                    <li>Address: <br><br><input type="address" placeholder="Address"></li>
+                    
+                </ul>
+                <button id="btn" type="submit" name="submit">
+                
+            </form>
+            
+        </div>
+        <div class="foot2">
+            <ul>
+                <li id="head2">ENGAGE & EXPERIENCE</li>
+                <li id="head3">213.894.9933</li>
+                <li id="head4">911 W. Washington Blvd <br> 
+                    Los Angeles, CA 90015 U.S.A.</li>
+                <li id="gre">hello@spinxdigital.com</li>
+            </ul>
+        </div>
+        
+    </section>
+</body>
+</html>
