418e8832dc3c24be3a55fd1d1afb281c9c9e4b2e
Bhuvan Y E <74106022+bhuvan-tech@users.noreply.github.com>
2021-11-01 19:52:44 +0530

    update

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SpinxDigital-Clone</title>
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    
+    <section class="one">
+        <nav class="first">
+            <span>
+                <img src="images/spinx-logo.png" alt="logo"></img>
+
+            </span>
+            <ul>
+                <li><a href="#">work</a></li>
+                <li><a href="#">expertise</a></li>
+                <li><a href="#">process</a></li>
+                <li><a href="#">agency</a></li>
+                <li><a href="#">resources</a></li>
+                <li><a href="#" id="contact-info">contact</a></li>
+    
+            </ul>
+        </nav>
+        <div class="bigheading">
+            <div id="big">BIG IDEAS,</div>
+            <div id="big">CREATIVE PEOPLE,</div>
+            <div id="big">NEW TECHNOLOGY.</div>
+        </div>
+    </section>
+    <section class="two">
+        <section class="s2d1">
+            <div class="big-heading">
+                <h1>THINK</h1>
+                <h1 id="gre">BIG.</h1>
+            </div>
+            <h2>Web Design Company in Los Angeles</h2>
+            <p>
+                At SPINX Digital, our team of innovators and digital marketers in tech and design bring skills above and beyond the ordinary to every project. Our Los Angeles web design and development team is involved in professional B2B and B2C web design and development, mobile applications, digital strategy, user experience, advertising, social media, content management systems, and email marketing initiatives.
+            </p>
+            <p id="gre">AWARD WINNING DIGITAL AGENCY</p>
+        </section>
+        <section class="s2d2">
+            <div class="line">
+                <div class="head2">PHYSICIATIONS FORMULA</div>
+                <div>
+                    <img src="images/icon-arrow-green.svg" alt="images">
+                    <img src="images/icon-arrow-green.svg" alt="images">
+                </div>
+            </div>
+            <div class="imgover">
+                <img src="images/best_in_class_2016_round.svg">
+            </div>
+            
+        </section>
+        
+    </section>
+    <section class="three">
+        <div>
+            <div class="forgreen">
+                <h1>RECENT WORK</h1>
+            </div>
+            
+            <p>We have a deep understanding of user habits and behaviors. We are committed to using fact-based knowledge and our unique brand of innovation to help you dominate the competition. Every project is customized for optimum visibility and audience response.</p>
+        </div>
+        <div class="imgs">
+            <div>
+                <img src="images/image-qe.jpg" id="exp" alt="">
+            </div>
+            <div>
+                <img src="images/image-AIYA.jpg" id="exp" alt="">
+            </div>
+            <div>
+                <img src="images/we-believe-idea-bg.jpg"  alt="">
+            </div>
+            <div>
+                <img src="images/image-qe.jpg" id="exp" alt="">
+            </div>
+            <div>
+                <img src="images/image-AIYA.jpg" id="exp" alt="">
+            </div>
+            <div>
+                <img src="images/we-believe-idea-bg.jpg" alt="">
+            </div>
+        </div>   
+        <button id="btn">View More</button>
+           
+    </section>
+    <section class="four">
+        <div class="forgreen">
+            <h2>SELECT CLIENTS</h2> 
+        </div>
+        <p> 
+            We are proud to have some of the most well-known and respected corporate clients – 
+            but we love to help start-ups and new brands too. We aren’t just talk. We are in the game, in 
+            a big way, and are ready to partner with you.
+        </p>
+        <div class="flex-images">
+            <div class="row1">
+                <img src="images/Logo1.png" alt="">
+                <img src="images/Logo2.png" alt="">
+                <img src="images/Logo3.png" alt="">
+                <img src="images/Logo4.png" alt="">
+                <img src="images/Logo5.png" alt="">
+                <img src="images/Logo6.png" alt="">
+            </div>
+            <div class="row2">
+                <img src="images/Logo7.png" alt="">
+                <img src="images/Logo8.png" alt="">
+                <img src="images/Logo9.png" alt="">
+                <img src="images/Logo10.png" alt="">
+                <img src="images/Logo11.png" alt="">
+                <img src="images/Logo12.png" alt="">
+            </div>
+        </div>
+    </section>
+
+
+    <section class="five">
+        <div class="forgreen">
+            <h1>
+            EXPERTISE
+            </h1>
+        </div>
+            <p id="para">
+                We are innovators in creative web design in Los Angeles. 
+                We don’t play any guessing games,but seek the facts from a range of 
+                analytics to help you get the edge.
+            </p>
+        <div class="flex6e">
+            <div class="ele">
+            
+                <img id="emg" src="images/icon-arrow-green.svg" alt="images">
+                <h2>DISCOVERY</h2>
+                <p>
+                voluptatibus porro debitis, dolore vero voluptates et id vel provident fugit perferendis eaque sunt doloribus dignissimos minus dolorem non! Ex fuga, recusandae eaque nemo quod illum.
+                </p>
+                <a href="#" id="gre">EXPLORE SERVICES</a>
+            </div>
+            <div class="ele">
+            
+                <img id="emg" src="images/icon-arrow-green.svg" alt="images">
+                <h2>STRATERFGY & <br> PLANNING</h2>
+                <p>
+                    voluptatibus porro debitis, dolore vero voluptates et id vel provident fugit perferendis eaque sunt doloribus dignissimos minus dolorem non! Ex fuga, recusandae eaque nemo quod illum.
+                </p>
+                <a href="#" id="gre">EXPLORE SERVICES</a>
+            </div>
+            <div class="ele">                    
+                <img id="emg" src="images/icon-arrow-green.svg" alt="images">
+                <h2>WEBSITE <br> SOLUTIONS</h2>
+                <p>
+                voluptatibus porro debitis, dolore vero voluptates et id vel provident fugit perferendis eaque sunt doloribus dignissimos minus dolorem non! Ex fuga, recusandae eaque nemo quod illum.
+                </p>
+                <a href="#" id="gre">EXPLORE SERVICES</a>
+            </div>
+            <div class="ele">
+                <img id="emg" src="images/icon-arrow-green.svg" alt="images">
+                <h2>MOBILE & TABLET <br> SOLUTIONS</h2>
+                <p>
+                voluptatibus porro debitis, dolore vero voluptates et id vel provident fugit perferendis eaque sunt doloribus dignissimos minus dolorem non! Ex fuga, recusandae eaque nemo quod illum.
+                </p>
+                <a href="#" id="gre">EXPLORE SERVICES</a>
+            </div>
+            <div class="ele">
+            
+                <img id="emg" src="images/icon-arrow-green.svg" alt="images">
+                <h2>ENTERPRISE CMS <br> SOLUTIONS</h2>
+                <p>
+                voluptatibus porro debitis, dolore vero voluptates et id vel provident fugit perferendis eaque sunt doloribus dignissimos minus dolorem non! Ex fuga, recusandae eaque nemo quod illum.
+                </p>
+                <a href="#" id="gre">EXPLORE SERVICES</a>
+            </div>
+            <div class="ele">
+            
+                <img id="emg" src="images/icon-arrow-green.svg" alt="images">
+                <h2>DIGITAL <br> MARKETING</h2>
+                <p>
+                    voluptatibus porro debitis, dolore vero voluptates et id vel provident fugit perferendis eaque sunt doloribus dignissimos minus dolorem non! Ex fuga, recusandae eaque nemo quod illum.
+                </p>
+                <a href="#" id="gre">EXPLORE SERVICES</a>
+            </div>
+        </div>
+       
+        <!-- <div class="carousel" data-flickity='{"wrapAround": true,"autoPlay":true}'>
+            <div class="carousel-cell">
+                <img class="carousel-cell-image"
+                src="images/icon-java.svg" />
+            </div>
+            <div class="carousel-cell">
+                <img class="carousel-cell-image"
+                src="images/icon-android.svg">
+            </div>
+            <div class="carousel-cell">
+                <img class="carousel-cell-image"
+                src="images/icon-magento.svg" />
+            </div>
+            <div class="carousel-cell">
+                <img class="carousel-cell-image"
+                src="images/icon-android.svg" />
+            </div>
+            <div class="carousel-cell">
+                <img class="carousel-cell-image"
+                src="images/icon-wordpress.svg" />
+            </div>
+            <div class="carousel-cell">
+                <img class="carousel-cell-image"
+               src="images/icon-dot-net.svg" />
+            </div>
+            <div class="carousel-cell">
+                <img class="carousel-cell-image"
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/tulip.jpg" alt="tulip" />
+            </div>
+            <div class="carousel-cell">
+                <img class="carousel-cell-image"
+                src="images/shore.jpg" alt="grapes" />
+            </div>
+  
+        </div> -->
+
+    </section>
+
+
+
+    <section class="last">
+        <div class="foot1">
+            <h1 id="gre">CONTACT US</h1>
+            <form action="registeration_form.php" method="post">
+                <ul>
+                    <li>First Name * <br><br> <input type="name" placeholder="Name"></li>
+                    <li>Last Name * <br><br> <input type="name" placeholder="Name"></li>
+                    
+                    <li>Email * <br><br><input type="email" placeholder="Email"></li>
+                    <li>Company Name <br><br><input type="email" placeholder="Email"></li>
+                    <li>Phone Number: <br><br><input type="phone number" placeholder="Phone Number"></li>
+
+                    <li>Project Type Product * <br><br>
+                        <select name="Project Type Product" aria-placeholder="Please Select">
+                            
+                        </select>
+                    </li>
+                    <li>Address: <br><br><input type="address" placeholder="Address"></li>
+                    
+                </ul>
+                <div class="btncenter">
+                    <button id="btn1" type="submit" name="submit">Submit</button>
+                </div>
+                
+                
+            </form>
+            
+        </div>
+        <div class="foot2">
+            <ul>
+                <li id="head2">ENGAGE & EXPERIENCE</li>
+                <li id="head3">213.894.9933</li>
+                <li id="head4">911 W. Washington Blvd <br> 
+                    Los Angeles, CA 90015 U.S.A.</li>
+                <li id="gre">hello@spinxdigital.com</li>
+            </ul>
+        </div>
+        
+    </section>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+
+</script>
+<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+</html>
